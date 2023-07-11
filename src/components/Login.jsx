4ef8e0ad088860a187410aa4bd06c64cff15aab1
unknown <yazid1bernou@gmail.com>
2023-07-11 14:11:54 +0000

    continue the structure of Login with styled component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -17,6 +17,12 @@ const Login = () => {
                 <h1>Welcome to your profesional community ! </h1>
                 <img src="/images/login-hero.svg" alt="" />
              </Hero>
+             <Form>
+                <Google>
+                    <img  src="/images/google.svg" alt="" /> 
+                    Sign in with Google
+                </Google>
+             </Form>
            </Section>
        </Container>
   )
@@ -28,6 +34,8 @@ const Join =  styled.a``
 const SignIn =  styled.a``
 const Section = styled.section``
 const Hero = styled.div``
+const Form = styled.div``
+const Google =  styled.button``
 
 
 export default Login
